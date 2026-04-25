641914dfd5a3d8826bc5d52ce77ae764e56cbff9
kysong <kyoungran.song@ai.dlh.lu>
2026-04-25 19:05:34 +0000

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+
+    x = len(sys.argv) - 1
+    if x == 0:
+        print("0 arguments.")
+    elif x == 1:
+        print("1 argument:")
+    else:
+        print("{}".format(x), "arguments:")
+
+    for i in range(1, x + 1):
+        print("{}: {}".format(i, sys.argv[i]))
