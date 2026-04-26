1c7d46632bbee74de5cf0daf21dc70cdfbef2d56
kysong <kyoungran.song@ai.dlh.lu>
2026-04-26 11:17:37 +0000

    100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     if sys.argv[2] == "+":
@@ -24,4 +24,4 @@ if __name__ == "__main__":
         print("{} / {} = {}".format(a, b, i))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
