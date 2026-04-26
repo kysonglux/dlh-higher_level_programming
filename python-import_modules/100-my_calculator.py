ca53117db4b271d41930724f4877e00e429a6f7f
kysong <kyoungran.song@ai.dlh.lu>
2026-04-26 11:25:13 +0000

    100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-from calculator_1 import add, sub, mul, div
 import sys
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
 
@@ -9,17 +9,18 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     a = int(sys.argv[1])
+    op = sys.argv[2]
     b = int(sys.argv[3])
-    if sys.argv[2] == "+":
+    if op == "+":
         x = add(a, b)
         print("{} + {} = {}".format(a, b, x))
-    elif sys.argv[2] == "-":
+    elif op == "-":
         y = sub(a, b)
         print("{} - {} = {}".format(a, b, y))
-    elif sys.argv[2] == "*":
+    elif op == "*":
         z = mul(a, b)
         print("{} * {} = {}".format(a, b, z))
-    elif sys.argv[2] == "/":
+    elif op == "/":
         i = div(a, b)
         print("{} / {} = {}".format(a, b, i))
     else:
