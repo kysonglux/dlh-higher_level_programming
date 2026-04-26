c81454c58865e218356692ae362f025fc7e7cdd2
kysong <kyoungran.song@ai.dlh.lu>
2026-04-26 19:39:55 +0000

    100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 import sys
-from calculator_1 import add, sub, mul, div
+import calculator_1
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     operator = {
-        "+": add,
-        "-": sub,
-        "*": mul,
-        "/": div,
+        "+": calculator_1.add,
+        "-": calculator_1.sub,
+        "*": calculator_1.mul,
+        "/": calculator_1.div,
     }
     a = int(sys.argv[1])
     op = sys.argv[2]
