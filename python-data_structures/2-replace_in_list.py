4320f8adce0c6554397a3e22b63cd0428f5df18f
kysong <kyoungran.song@ai.dlh.lu>
2026-04-28 09:46:27 +0000

    2-replace_in_list.py

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,8 +2,8 @@
 
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) - 1:
-        return my_list.copy()
+        return my_list
     else:
-        new_list = my_list.copy()
+        new_list = my_list
         new_list[idx] = element
         return new_list
