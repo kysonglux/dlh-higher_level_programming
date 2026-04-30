3d0cdbbbf522895f2db374dce4860589b2fa6d79
kysong <kyoungran.song@ai.dlh.lu>
2026-04-30 11:02:59 +0000

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,8 @@
 
 def roman_to_int(roman_string):
     roman = roman_string
-    if not roman:
-        return None
+    if not isinstance(roman, str):
+        return 0
     dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     roman_number = 0
     for i in range(len(roman)):
