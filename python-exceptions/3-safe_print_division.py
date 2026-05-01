d40d32db3db7db64e1ecd0a23de5cf5a73b6849b
kysong <kyoungran.song@ai.dlh.lu>
2026-05-01 19:23:49 +0000

    3-safe_print_division.py

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    
+    result = None
+    try:
+        result = a / b
+    except (TypeError, ZeroDivisionError):
+        return None
+    finally:
+        print("Inside result:", "{}".format(result))
+    return result
