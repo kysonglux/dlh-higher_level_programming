583d3fc48ea7b8b6b973018f158638bb21c15bdd
kysong <kyoungran.song@ai.dlh.lu>
2026-05-05 10:46:19 +0000

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 class Square:
-    """A class that defines a square by size."""
-    def __init__(self):
-        """Initialize the square with a given size."""
-        pass
-
-    def __str__(self):
-        return "{}"
+    pass
