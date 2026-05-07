2c856b3d9a9f3f819b781626205905ddc292d28e
kysong <kyoungran.song@ai.dlh.lu>
2026-05-07 17:07:20 +0000

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python3
-""" Class Square"""
+""" Create Class Square"""
 
 
 class Square:
-    """ """
-    def __init__(self, size):
-        self__size = size
+    """ about the square class"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
 
     @property
     def size(self):
         return self__size
     
-    @get_setter
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
     
+    @property
+    def position(self):
+        return self.__position = position
+    
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple)
+            raise TypeError("position must be a tuple of 2 positive integers")
+        
