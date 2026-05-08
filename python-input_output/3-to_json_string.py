1a07681919a1373aedb452b1157a5e427d51493b
kysong <kyoungran.song@ai.dlh.lu>
2026-05-08 13:24:28 +0000

    3-to_json_string.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""what is json(JavaScript Object Notation)"""
+import json
+
+
+def to_json_string(my_obj):
+    """Return the Json string representation of a python object. """
+    return json.dumps(my_obj)
