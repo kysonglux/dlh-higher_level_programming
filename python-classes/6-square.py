f7982406711ad82ac571251392fcbaeb8a823cfe
kysong <kyoungran.song@ai.dlh.lu>
2026-05-08 06:38:28 +0000

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -10,8 +10,8 @@ class Square:
 
     @property
     def size(self):
-        return self__size
-    
+        return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -19,13 +19,32 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     @property
     def position(self):
-        return self.__position = position
-    
+        return self.__position
+
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple)
+        if (
+            not isinstance(value, tuple) or
+            len(value) != 2 or
+            not all(isinstance(num, int) for num in value) or
+            not all(num >= 0 for num in value)
+        ):
             raise TypeError("position must be a tuple of 2 positive integers")
-        
+        self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+            return
+
+        for _ in range(self.__position[1]):
+            print("")
+
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
