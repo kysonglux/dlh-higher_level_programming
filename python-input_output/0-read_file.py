6aa2961697f27bd8bc763df12714f528caeba7fc
kysong <kyoungran.song@ai.dlh.lu>
2026-05-08 09:34:49 +0000

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""how to read file"""
+"""how to read files"""
 
 
 def read_file(filename="my_file_0"):
+    """using parameter, read a file and print out """
     with open(filename, encoding="utf-8") as f:
         print(f.read())
