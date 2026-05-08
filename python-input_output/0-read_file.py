d4a64ff07ec7f2e9670de14e3bb3459beb6c2356
kysong <kyoungran.song@ai.dlh.lu>
2026-05-08 11:36:55 +0000

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename="my_file_0"):
     """using parameter, read a file and print out """
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
