e89313f77a159d28d3a47bb23b5057cbb99916d4
kysong <kyoungran.song@ai.dlh.lu>
2026-05-09 07:06:18 +0200

    task_00_basic_serialization.py

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,19 @@
+#!/user/bin/env python3
+""" serialization and deserializaation """
+
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """serialize"""
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f)
+    pass
+
+
+def load_and_deserialize(filename):
+    """deserialize"""
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
+    pass
