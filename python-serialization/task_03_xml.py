aacbf091ea29b3b5154eef060e14704300a35b76
kysong <kyoungran.song@ai.dlh.lu>
2026-05-09 21:39:08 +0200

    task_03_xml.py

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""serializing and deserializing with xml"""
+
+
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+    """ serialize the dictionary into xml """
+    try:
+        root = ET.Element("data")
+
+        for key, value in dictionary.items():
+            child = ET.SubElement(root, key)
+            child.text = str(value)
+        tree = ET.ElementTree(root)
+        tree.write(filename, encoding="utf-8", xml_declaration=True)
+
+        return True
+    except Exceotion:
+        return False
+
+
+def deserialize_from_xml(filename):
+    """read the xml and return a deserialized dictionary"""
+    try:
+        tree = ET.parse(filename)
+        root = tree.getroot()
+
+        result = {}
+        for child in root:
+            result[child.tag] = child.text
+
+        return result
+
+    except Exception:
+        return None
+
+
+"""def main():
+    sample_dict = {
+        'name': 'John',
+        'age': '28',
+        'city': 'New York'
+    }
+
+    xml_file = "data.xml"
+    serialize_to_xml(sample_dict, xml_file)
+    print(f"Dictionary serialized to {xml_file}")
+
+    deserialized_data = deserialize_from_xml(xml_file)
+    print("\nDeserialized Data:")
+    print(deserialized_data)
+
+if __name__ == "__main__":
+    main()"""
