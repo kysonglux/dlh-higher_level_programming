d0d535ebac1879693aded25ddd25e3d7f54aa126
kysong <kyoungran.song@ai.dlh.lu>
2026-05-09 18:07:16 +0200

    task_02_csv.py

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""converting csv data to json format"""
+
+
+import csv
+import json
+import os
+
+
+def convert_csv_to_json(csv_filename):
+    """Convert CSV file to JSON file.
+    Return true on success, False on failure."""
+    if not os.path.exists(csv_filename):
+        return Flase
+    try:
+        with open(csv_filename, mode='r', encoding='utf-8') as csvfile:
+            data = list(csv.DictReader(csvfile))
+
+        with open(json_filename, mode='w', encoding='utf-8') as jsonfile:
+            json.dump(data, jsonfile, indent=4)
+        return True
+
+    except Exception:
+        return False
