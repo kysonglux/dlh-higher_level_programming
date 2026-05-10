1c44e0ba973d117c45b5c6a3e69602658d6f66e8
kysong <kyoungran.song@ai.dlh.lu>
2026-05-10 19:03:29 +0000

    12-pascal_triangle.py

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""create Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    triangle = []
+
+    for i in range(n):
+        if i <= 0:
+            triangle.append([1])
+        else:
+            prev = triangle[-1]
+            row = [1]
+
+            for j in range(len(prev) - 1):
+                row.append(prev[j] + prev[j + 1])
+            row.append(1)
+            triangle.append(row)
+    return triangle
